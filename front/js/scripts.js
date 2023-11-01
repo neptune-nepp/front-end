7b7f2e1025c7cddb5f8e8196f7122eb5090e78e4
nene <belavezzu@gmail.com>
2023-11-01 11:00:32 -0300

    Aula 01/11 até código 2.13.4

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -1,10 +1,49 @@
-const protocolo = 'http://';
-const baseURL = 'localhost:3000';
-const filmesEndPoint = '/filmes';
+const protocolo = "http://"
+const baseURL = "localhost:3000"
+const filmesEndPoint = "/filmes"
 
 async function obterFilmes(){
-    // console.log("Tá fununsando meu querido")
-    const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`;
-    const filmes = (await axios.get(URLcompleta)).data;
-    console.log(filmes);
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`
+    const filmes = (await axios.get(URLcompleta)).data
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    for(let filme of filmes){
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
+}
+async function cadastrarFilme(){
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndPoint}`
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput')
+    let titulo = tituloInput.value
+    let sinopse = sinopseInput.value
+    tituloInput.value = ""
+    sinopseInput.value = ""
+
+    if (titulo && sinopse){
+        const filmes = (await axios.post(URLcompleta, {titulo,sinopse})).data
+        let tabela = document.querySelector('.filmes')
+        let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+        corpoTabela.innerHTML = ""
+        for(let filme of filmes){
+            let linha = corpoTabela.insertRow(0)
+            let celulaTitulo = linha.insertCell(0)
+            let celulaSinopse = linha.insertCell(1)
+            celulaTitulo.innerHTML = filme.titulo
+            celulaSinopse.innerHTML = filme.sinopse
+        }
+    }
+    else{
+        let alert = document.querySelector(".alert");
+        alert.classList.add("show");
+        alert.classList.remove("d-none")
+        setTimeout(() => {
+            alert.classList.add("d-none");
+            alert.classList.remove("show");
+        }, 2000)
+    }
 }
