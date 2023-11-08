f759041458113f877bb54e5995139820aaca09b4
nene <belavezzu@gmail.com>
2023-11-08 10:41:01 -0300

    Aula 10 pot.3

--- Aula 10/backend.js
+++ Aula 10/backend.js
@@ -1,10 +1,7 @@
-// String de conexão com o banco
-//mongodb+srv://belavezzu:netuno21@cluster0.gtf9lcp.mongodb.net/?retryWrites=true&w=majority
-
-
 const express = require ('express')
 const cors = require('cors');
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -13,6 +10,14 @@ const Filme = mongoose.model("filme", mongoose.Schema({
     titulo: {type:String},
     sinopse: {type:String}
 }));
+
+const usuarioSchema = mongoose.Schema({
+    login: {type:String, required: true, unique: true},
+    senha: {type:String, required: true}
+})
+usuarioSchema.plugin(uniqueValidator);
+
+const usuario = mongoose.model("Usuário", usuarioSchema);
 
 async function conectarMongo() {
     await mongoose.connect (`mongodb+srv://belavezzu:netuno21@cluster0.gtf9lcp.mongodb.net/?retryWrites=true&w=majority`)
@@ -50,6 +55,18 @@ app.post('/filmes', async (req, res) => {
     // carregar a base atualizada
     const filmes = await Filme.find(); //find é um método de classe
     res.json(filmes);
+})
+
+app.post('/signup', async (req, res) => {
+    const login = req.body.login;
+    const senha = req.body.senha;
+    const usuario = new Usuario({
+        login: login,
+        senha: senha
+    })
+    const respostaDoMongo = await usuario.save();
+    console.log(respostaDoMongo);
+    res.end();
 })
 
 app.listen(3000, () => {
